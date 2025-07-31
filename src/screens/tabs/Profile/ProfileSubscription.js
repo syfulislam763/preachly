efe3626183002e559f0871076f23224ee26fab72
Syful <syful@Bdcallings-Mac-mini-3.local>
2025-07-31 06:27:59 +0600

    payment method error

--- src/screens/tabs/Profile/ProfileSubscription.js
+++ src/screens/tabs/Profile/ProfileSubscription.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, ImageBackground, StyleSheet, Image, Dimensions} from 'react-native';
 import { useAuth } from '../../../context/AuthContext';
 import {
@@ -11,134 +11,96 @@ import PlanSelector from '../../../components/SubscriptionPlan';
 import CustomHeader from '../../../components/CustomNavigation';
 import ParagraphIcon from '../../../components/ParagraphIcon';
 
-import {
-  presentGooglePay,
-  isGooglePaySupported,
-  initGooglePay,
-} from '@stripe/stripe-react-native';
+import { StripeProvider } from '@stripe/stripe-react-native';
+import { CardField, useStripe } from '@stripe/stripe-react-native';
+import CustomModal from '../../../components/CustomModal';
+import PaymentScreen from './payment/PaymentScreen';
 
 const window = Dimensions.get("window")
 
 export default function ProfileSubscription({ navigation }) {
   const { login } = useAuth();
 
-
-  const initialize = async () => {
-      const isSupported = await isGooglePaySupported();
-      if (!isSupported) {
-        console.log('Google Pay not supported on this device');
-        return;
-      }
-
-      const { error } = await initGooglePay({
-        testEnv: true,
-        merchantName: 'My Test Merchant',
-        countryCode: 'US',
-        billingAddressConfig: {
-          isRequired: false,
-          format: 'MIN',
-          isPhoneNumberRequired: false,
-        },
-        existingPaymentMethodRequired: false,
-      });
-
-      if (error) {
-        console.log('Init Error', error.message);
-      }
-    };
-
-
-  useEffect(() => {
-    console.log("init")
-    initialize();
-  }, [])
-
-
-  const payWithGooglePay = async () => {
-    try {
-      // const response = await fetch('http://192.168.0.101:8000/create-payment-intent', {
-      //   method: 'POST',
-      //   headers: { 'Content-Type': 'application/json' },
-      //   body: JSON.stringify({ amount: 1000, currency: 'usd' }),
-      // });
-      // const { clientSecret } = await response.json();
-      const clientSecret = "pi_3NF9w3S9d2ABC123_secret_G2m8X45Y9zz9jHkV7gkJGKkPj"
-
-      const result = await presentGooglePay({ clientSecret });
-
-      if (result.error) {
-        console.log('Error', result.error.message);
-      } else {
-        console.log('Success', 'Payment completed!');
-      }
-    } catch (error) {
-      console.log('Error', error.message);
-    }
-  };
+  const [openPayment, setOpenPayment] = useState(false);
 
   
 
+
+  const key = `pk_test_51RVMTHQU9tGM4LXBf8ZHLjC18DYzzWu4HnxSCojMGP58ZO8x1K2sFbNZ5xGLmIRt6KjZpo77V0RKs4m6dwoxoFLi00u06pnafX`
+
   return (
-    <View style={{ flex:1, backgroundColor:'#fff', justifyContent:'space-between'}}>
+    <StripeProvider publishableKey={key}>
+        <View style={{ flex:1, backgroundColor:'#fff', justifyContent:'space-between'}}>
+          
+
+
+          <View style={styles.content}>
+              
+              <Text style={{
+                  fontFamily:'DMSerifDisplay',
+                  fontSize: 28,
+                  color:'#0B172A',
+                  paddingBottom: 25
+              }}>Your Plan</Text>
+
+              <ParagraphIcon
+                icon={require("../../../../assets/img/24-sunset.png")}
+                text={"Build Confidence in Conversations About Faith"}
+                textStyle={{fontFamily:'NunitoSemiBold'}}
+              />
+              <ParagraphIcon
+                icon={require("../../../../assets/img/bird.png")}
+                text={"Clarity and Ease When You Need It Most"}
+                textStyle={{fontFamily:'NunitoSemiBold'}}
+              />
+              <ParagraphIcon
+                icon={require("../../../../assets/img/piramid.png")}
+                text={"Inspire and Strengthen Your Walk with God"}
+                textStyle={{fontFamily:'NunitoSemiBold'}}
+              />
+
+              <View style={{height:20}}></View>
+
+              <PlanSelector
+                  OtherPlan={() => <Text style={{
+                      fontSize:20,
+                      color: '#0000000',
+                      fontWeight:'500',
+                      paddingBottom: 15
+                      }}>Other Plans</Text>
+                  }
+              />
+
+              <View style={{height:30}}></View>
+
+          </View>
+
+          <View style={{
+              padding:20
+          }}>
+              <CommonButton
+                  btnText={"Manage subscription"}
+                  bgColor={"#005A55"}
+                  navigation={navigation}
+                  route={""}//"SubscriptionConfirmedScreen"
+                  handler={() => setOpenPayment(true)}
+                  txtColor={"#fff"}
+                  opacity={1}
+              />
+          </View>
+
          
 
-
-        <View style={styles.content}>
-            
-            <Text style={{
-                fontFamily:'DMSerifDisplay',
-                fontSize: 28,
-                color:'#0B172A',
-                paddingBottom: 25
-            }}>Your Plan</Text>
-
-            <ParagraphIcon
-              icon={require("../../../../assets/img/24-sunset.png")}
-              text={"Build Confidence in Conversations About Faith"}
-              textStyle={{fontFamily:'NunitoSemiBold'}}
-            />
-            <ParagraphIcon
-              icon={require("../../../../assets/img/bird.png")}
-              text={"Clarity and Ease When You Need It Most"}
-              textStyle={{fontFamily:'NunitoSemiBold'}}
-            />
-            <ParagraphIcon
-              icon={require("../../../../assets/img/piramid.png")}
-              text={"Inspire and Strengthen Your Walk with God"}
-              textStyle={{fontFamily:'NunitoSemiBold'}}
-            />
-
-            <View style={{height:20}}></View>
-
-            <PlanSelector
-                OtherPlan={() => <Text style={{
-                    fontSize:20,
-                    color: '#0000000',
-                    fontWeight:'500',
-                    paddingBottom: 15
-                    }}>Other Plans</Text>
-                }
-            />
-
-            <View style={{height:30}}></View>
-
-        </View>
-
-        <View style={{
-            padding:20
-        }}>
-            <CommonButton
-                btnText={"Manage subscription"}
-                bgColor={"#005A55"}
-                navigation={navigation}
-                route={""}//"SubscriptionConfirmedScreen"
-                handler={() => payWithGooglePay()}
-                txtColor={"#fff"}
-                opacity={1}
-            />
-        </View>
-
-    </View>
+          <CustomModal 
+            modalContainerStyle={{height:"auto"}} 
+            visible={openPayment} 
+            onClose={() => setOpenPayment(false)}
+            headerStyle={{paddingRight:10}}
+          >
+            <PaymentScreen/>
+          </CustomModal>
+      </View>
+    </StripeProvider>
   );
 }
 
